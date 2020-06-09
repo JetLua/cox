2ef20f37d5b9a7930b5f87d6135fcc147aac9dab
JetLu <i@lufei.im>
2020-06-09 22:29:55 +0800

    update

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,27 @@
+import {terser} from 'rollup-plugin-terser'
 import ts from '@rollup/plugin-typescript'
 
 export default {
   input: './src/index.ts',
   plugins: [
-    ts()
+    ts(),
+    terser({
+      output: {
+        comments: false
+      }
+    })
   ],
-  output: {
-    format: 'es',
-    sourcemap: true,
-    file: 'dist/cox.es.js'
-  }
+  output: [
+    {
+      format: 'es',
+      sourcemap: true,
+      file: 'dist/cox.es.js'
+    },
+    {
+      format: 'umd',
+      sourcemap: true,
+      name: 'cox',
+      file: 'dist/cox.min.js'
+    }
+  ]
 }
